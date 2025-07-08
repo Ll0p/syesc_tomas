64940cf101368828b32907decd8e4a54a5905021
Tomas Llop <tomasignaciollop@gmail.com>
2025-07-08 14:07:45 -0300

    f

--- Modulos/datos_jugador.py
+++ Modulos/datos_jugador.py
@@ -5,7 +5,7 @@ def crear_estado_jugador(preguntas: list[dict]) -> dict:
         "estado" : "Menu",
         "usuario" : "",
         "posicion" : 15,
-        "contador" : 10,
+        "contador" : 15,
         "rect_posicion": pygame.Rect(254,105,25,25),
         "respuesta" : None,
         "mensaje_error" : "",
